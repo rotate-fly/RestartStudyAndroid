bcdb12007f550c54e74251cf4bd67548ba7e45f4
huangzisheng <huangzs@aheading.com>
2019-04-09 15:55:45 +0800

    Initial commit

---
+++ app/src/androidTest/java/fly/rotate/com/restartstudyandroid/ExampleInstrumentedTest.kt
@@ -0,0 +1,24 @@
+package fly.rotate.com.restartstudyandroid
+
+import android.support.test.InstrumentationRegistry
+import android.support.test.runner.AndroidJUnit4
+
+import org.junit.Test
+import org.junit.runner.RunWith
+
+import org.junit.Assert.*
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+@RunWith(AndroidJUnit4::class)
+class ExampleInstrumentedTest {
+    @Test
+    fun useAppContext() {
+        // Context of the app under test.
+        val appContext = InstrumentationRegistry.getTargetContext()
+        assertEquals("fly.rotate.com.restartstudyandroid", appContext.packageName)
+    }
+}
