8a462d0a03edbd749c3bf5e9053ef2020a62e243
huangzisheng <huangzs@aheading.com>
2019-04-16 15:46:11 +0800

    【常用控件】Button系列

--- app/src/main/java/hzs/robotwing/com/view/ButtonActivity.java
+++ app/src/main/java/hzs/robotwing/com/view/ButtonActivity.java
@@ -1,15 +1,33 @@
 package hzs.robotwing.com.view;
 
+import android.app.Activity;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
-import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.MotionEvent;
+import android.view.PointerIcon;
+import android.view.View;
 import android.widget.*;
 import hzs.robotwing.com.R;
 
 /**
  * @author hzs
  * <p>
+ *     Button 继承TextView
+ *
+ *        @Override
+ *     public PointerIcon onResolvePointerIcon(MotionEvent event, int pointerIndex) {
+ *         if (getPointerIcon() == null && isClickable() && isEnabled()) {
+ *             return PointerIcon.getSystemIcon(getContext(), PointerIcon.TYPE_HAND);
+ *         }
+ *         return super.onResolvePointerIcon(event, pointerIndex);
+ *     }
+ * Android 7.0 引入 Custom Pointer API，以便您可以自定义指针的外观、 可见性和行为。
+ * 此功能在用户使用鼠标或触控板与 UI 对象交互尤为有用。默认指针使用标准图标。
+ * 此 API 还包含多种高级功能，例如根据鼠标或触控板特定移动情况改变指针图标外观。
+ * 要设置指针图标，请替换 View 类的 onResolvePointerIcon() 方法。
+ * 此方法使用 PointerIcon 对象绘制与特定移动事件对应的图标。
  * <p>
  * <p>
  * Switch控件：
@@ -23,9 +41,15 @@ import hzs.robotwing.com.R;
  * * @attr ref android.R.styleable#Switch_track   底部的Drawable
  * <p>
  * Switch控件在不同系统手机上是样式差异较大，一般使用自定义的SwitchButton控件
+ *
+ * 常见问题
+ * 1、防止按钮重复点击：https://blog.csdn.net/hust_twj/article/details/78742453
+ *     时间使用SystemClock类 SystemClock.elapsedRealtime方法
+ *
  */
-public class ButtonActivity extends AppCompatActivity {
+public class ButtonActivity extends Activity {
 
+    private Button btn;
     private RadioGroup radioGroup;
     private RadioButton radio0;
     private RadioButton radio1;
@@ -50,6 +74,7 @@ public class ButtonActivity extends AppCompatActivity {
     }
 
     private void initView() {
+        btn = findViewById(R.id.btn);
         radioGroup = (RadioGroup) findViewById(R.id.radio_group);
         radio0 = (RadioButton) findViewById(R.id.radio0);
         radio1 = (RadioButton) findViewById(R.id.radio1);
@@ -68,6 +93,26 @@ public class ButtonActivity extends AppCompatActivity {
 
 
     private void initEvent() {
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+            btn.setPointerIcon(PointerIcon.getSystemIcon(this,1000));
+        }
+        btn.setOnTouchListener(new View.OnTouchListener() {
+
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                switch (event.getAction()) {
+                    case MotionEvent.ACTION_DOWN:
+                        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+//                            btn.onResolvePointerIcon(event,R.drawable.bg_switch_track_false);
+                        }
+                        break;
+                    default:
+                        break;
+                }
+                return false;
+            }
+        });
         radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(RadioGroup group, int checkedId) {
